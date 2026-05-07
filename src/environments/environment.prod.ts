14e3f68838251babbb790409238c3c73366efe57
Konstantinos Spyrou <spyroukon@gmail.com>
2026-05-07 14:16:22 +0300

    build(environment): add environment file for production build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  API_ENDPOINT: '/node',
+  API_LOGIN: '/node/oauth2/authorization/eosc',
+  API_LOGOUT: '/node/logout',
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
+  production: false,
   API_ENDPOINT: '/api',
   API_LOGIN: '/api/oauth2/authorization/eosc',
-  API_LOGOUT: '/api/logout',
-  production: true
+  API_LOGOUT: '/api/logout'
 };
